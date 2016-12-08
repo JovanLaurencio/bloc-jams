df5af29979e8e7c009dfc9128361fe23660fbb6e
Jovan Laurencio <jovan@itsjovan.com>
2016-12-08 17:27:47 -0600

    Update landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-var animatePoints = function() {
- 
-                 var points = document.getElementsByClassName('point');
- 
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
-=======
  var animatePoints = function() {
  
      var points = document.getElementsByClassName('point');
@@ -46,4 +14,3 @@ var animatePoints = function() {
 	   revealPoint(i);	 
 	 }
  };
->>>>>>> 89e8d9248e4d2a240e080934177a7e4349cb990f
